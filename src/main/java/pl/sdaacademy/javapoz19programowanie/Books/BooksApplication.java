d4f6d11b9861551f64086bb1377f85ea3504b0ec
Mariusz Marczewski <mariusz.marczewski1@poczta.fm>
2019-08-04 14:50:50 +0200

    initial structure of application

---
+++ src/main/java/pl/sdaacademy/javapoz19programowanie/Books/BooksApplication.java
@@ -0,0 +1,7 @@
+package pl.sdaacademy.javapoz19programowanie.Books;
+
+public class BooksApplication {
+    public static void main(String[] args) {
+        new BooksStart().start();
+    }
+}
---
+++ src/main/java/pl/sdaacademy/javapoz19programowanie/Books/BooksViews.java
@@ -0,0 +1,21 @@
+package pl.sdaacademy.javapoz19programowanie.Books;
+
+import java.util.Scanner;
+
+public class BooksViews {
+
+    private Scanner scanner;
+
+    public BooksViews(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int startMenu(){
+        System.out.println("1. Authors");
+        System.out.println("2. Books");
+        System.out.println("0. Koniec");
+        int menu = scanner.nextInt();
+        scanner.nextLine();
+        return menu;
+    }
+}
